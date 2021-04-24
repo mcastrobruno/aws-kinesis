e9dceefccf8763a0d6558892d822f886bc3d8cd0
Bruno Castro <mcastrobruno@gmail.com>
2021-04-24 09:44:04 +0100

    Initial Commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aws_kinesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aws Kinesis!");
+        }
+    }
+}
